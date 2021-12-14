e4c2eddb574fd96e9da3c9ec92168180c6603118
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2021-12-14 15:51:18 +0430

    useRef (focus on typing section added)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./styles.css";
 
 function App() {
+	const STARTING_TIME = 5;
 	const [text, setText] = useState("");
-	const [timeRemaining, setTimeRemaining] = useState(5);
+	const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
 	const [isTimeRunning, setIsTimeRunning] = useState(false);
-	console.log(isTimeRunning);
+	const [wordCount, setWordCount] = useState(0);
+	const textBoxRef = useRef(null); // a way to access the DOM
 
 	function handleChange(e) {
 		const { value } = e.target;
@@ -16,29 +18,43 @@ function App() {
 		const wordsArr = text.trim().split(" ");
 		return wordsArr.filter((word) => word !== "").length;
 	}
+
+	function startGame() {
+		setTimeRemaining(STARTING_TIME);
+		setIsTimeRunning(true);
+		setText("");
+		textBoxRef.current.disabled = false;
+		textBoxRef.current.focus();
+	}
+	function endGame() {
+		setIsTimeRunning(false);
+		setWordCount(calculateWordCount(text));
+	}
 	useEffect(() => {
 		if (isTimeRunning && timeRemaining > 0) {
 			setTimeout(() => {
 				setTimeRemaining((time) => time - 1);
 			}, 1000);
-		} else if (!timeRemaining) {
-			setIsTimeRunning(false);
+		} else if (timeRemaining === 0) {
+			endGame();
 		}
 	}, [timeRemaining, isTimeRunning]);
 
 	return (
 		<div>
 			<h1>How fast do you type?</h1>
-			<textarea name="text" onChange={handleChange} value={text} />
+			<textarea
+				ref={textBoxRef}
+				name="text"
+				onChange={handleChange}
+				value={text}
+				disabled={!isTimeRunning}
+			/>
 			<h4>Time remaining: {timeRemaining}</h4>
-			<button
-				onClick={() => {
-					setIsTimeRunning((prevState) => !prevState);
-				}}
-			>
+			<button onClick={startGame} disabled={isTimeRunning}>
 				Start
 			</button>
-			<h1>Word count: ???</h1>
+			<h1>Word count: {wordCount}</h1>
 		</div>
 	);
 }
